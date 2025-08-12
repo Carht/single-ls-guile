e9d1764755cccefcb3a26966c53e0dc65c107db4
Charte Erbeth <charte_charte@proton.me>
2025-08-11 23:22:39 -0400

    #1 Realizando predicado para filtrar los archivos que tienen una determinada extensión, en este caso particular es útil listar los archivos con extensión PDF y los archivos con extensión EPUB.

--- test.scm
+++ test.scm
@@ -1,5 +1,27 @@
-(use-modules (ice-9 pretty-print))
+;; Valida si el directorio de entrada existe.
+(define (list-files directory-path)
+  (if (file-exists? directory-path)
+      (list-dir directory-path)
+      (format #t "Error el directorio no existe.")))
 
-(define lst (list-dir "/home/usuario/src/single-ls-guile/"))
-(pretty-print lst)
-(display (length lst)) (display " rutas encontradas\n")
+
+;; Extrae el segundo elemento de una lista, si es posible, retorna el elemento
+;; sino retorna una lista vacía.
+(define (second lst)
+  (if (>= (length lst) 2)
+      (cadr lst)
+      '()))
+
+;; Dada una ruta de directorio y una extensión, retorna la lista de todos
+;; los archivos que contengan dicha extensión.
+(define (files-with-extension directory-path extension)
+  (let ((all-files (list-files directory-path)))
+    (map (lambda (filename)
+	   (if (equal? (second (string-split (basename filename) #\.)) extension)
+	       (format #t "~a\n" filename)))
+	 all-files)))
+
+;; Imprime las rutas de los archivos contenidos en el directorio.
+(files-with-extension "/home/lisper/books/Lisp/" "pdf")
+(files-with-extension "/home/lisper/books/Lisp/" "epub")
+
